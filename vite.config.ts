bc44009fbde632e7901ecb81b902bc18de1a0d1d
Md Mahmud Hasan <108387443+rafsan3503@users.noreply.github.com>
2024-12-02 20:29:37 +0600

    moved to varcel

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-  server: {
-    host: true,
-    port: 3000
-  },
   build: {
     outDir: 'dist',
     sourcemap: true,
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  server: {
+    host: true,
+    port: 3000
   }
 }) 
